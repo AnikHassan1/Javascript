abb692a134741f8f34d9db1cf6799891e4edc1f0
Anik Hassan1 <ah491606@gmail.com>
2023-09-30 21:04:49 +0600

    first-commit

---
+++ Js/index.js
@@ -0,0 +1,18 @@
+
+document.getElementById("title").innerHTML="<h1> habiv</h1>";
+
+ var x =50;
+ var y ; //buy total tk
+
+y=500;
+ var  total = y - x;
+docu 
+var a=50;
+const b=20;
+a=4000;
+document.write(a+b);
+
+let s=34;
+let d=56;
+d=20;
+document.write(s-d);
